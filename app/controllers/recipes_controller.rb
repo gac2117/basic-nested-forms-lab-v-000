a1ccff6aeda7aa80f4a498c4ac2ca5d9c1058135
Grace Choi <oiograceoio@gmail.com>
2019-04-26 16:46:16 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,4 +14,15 @@ class RecipesController < ApplicationController
   def create
 
   end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    ingredients_attributes:[
+      :name,
+      :quantity
+    ]
+    )
+  end
 end
